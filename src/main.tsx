956619c84810f43deb1c6b39482f59047f358406
Juan Prieto <jp1739@gmail.com>
2025-05-21 08:12:27 -0500

    fix: delete .tsx extension

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import App from "./App.tsx";
-import { StoreProvider } from "./context/StoreProvider.tsx";
+import App from "./App";
+import { StoreProvider } from "./context/StoreProvider";
 
 createRoot(document.getElementById("root")!).render(
   <StoreProvider>
